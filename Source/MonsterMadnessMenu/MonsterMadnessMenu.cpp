93dd2447597d4c2f78566ad43c0a5bab8ddce84b
Batname <dadubinin@gmail.com>
2017-07-02 16:18:04 +0200

    changes in configs

--- Source/MonsterMadnessMenu/MonsterMadnessMenu.cpp
+++ Source/MonsterMadnessMenu/MonsterMadnessMenu.cpp
@@ -1,4 +1,20 @@
 #include "MonsterMadnessMenu.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(MonsterMadnessMenu);
+
+#define LOCTEXT_NAMESPACE "FMonsterMadnessMenu"
+
+void FMonsterMadnessMenu::StartupModule()
+{
+	UE_LOG(MonsterMadnessMenu, Warning, TEXT("MonsterMadnessMenu has started!!"));
+}
+
+void FMonsterMadnessMenu::ShutdownModule()
+{
+	UE_LOG(MonsterMadnessMenu, Warning, TEXT("MonsterMadnessMenu has shut down!!"));
+}
+
+#undef LOCTEXT_NAMESPACE
+
 IMPLEMENT_MODULE( FDefaultGameModuleImpl, MonsterMadnessMenu );
--- Source/MonsterMadnessMenu/MonsterMadnessMenu.h
+++ Source/MonsterMadnessMenu/MonsterMadnessMenu.h
@@ -2,3 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "Engine.h"
+#include "ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(MonsterMadnessMenu, All, All);
+
+class FMonsterMadnessMenu : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+
+	virtual void ShutdownModule() override;
+};
